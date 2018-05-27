ef70e11d5f9e2d506e922da78a6c88b91713801f
DanSytnikov <dan.sytnikov@gmail.com>
2018-05-27 12:03:21 +0300

    mks

---
+++ app/src/main/java/com/example/user/weather/WeatherAdapter.java
@@ -0,0 +1,12 @@
+package com.example.user.weather;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+public class WeatherAdapter extends RecyclerView {
+    public WeatherAdapter(Context context){
+        super(context);
+    }
+
+
+}
